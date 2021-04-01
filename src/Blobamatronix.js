dbc89e3ebd95a780fc9fd2bffea44fcf4aa97eba
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-01 15:46:16 +0800

    noise-generation Hide canvas

--- src/Blobamatronix.js
+++ src/Blobamatronix.js
@@ -56,7 +56,8 @@ class Blobamatronix {
    }
 
    generateTextureCanvas() {
-      this.canvas = document.getElementById('noise-canvas');
+      //this.canvas = document.getElementById('noise-canvas');
+      this.canvas = document.createElement('canvas');
       this.canvas.width = 250;
       this.canvas.height = 250;
       this.context = this.canvas.getContext('2d');
