4ce24aad1560ff38afbb65102fbb117669d72153
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-01 14:54:46 +0800

    noise-generation Get rid of the octaves since it seems to be slowing down shit

--- src/Blobamatronix.js
+++ src/Blobamatronix.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import SimplexNoise from 'simplex-noise';
 import { octave, map } from './Utilities';
 
 class Blobamatronix {
@@ -10,9 +11,9 @@ class Blobamatronix {
       this.camera = null;
       this.renderer = null;
       this.renderFrame = this.renderFrame.bind(this);
-      this.generateTexture();
+      this.generateTextureCanvas();
       //this.setupWorld();
-      //this.renderFrame();
+      this.renderFrame();
 
     }
 
@@ -54,31 +55,39 @@ class Blobamatronix {
 
    }
 
-   generateTexture() {
+   generateTextureCanvas() {
       this.canvas = document.getElementById('noise-canvas');
-      this.canvas.width = 400;
-      this.canvas.height = 400;
-      this.context = this.canvas.getContext('2d')
+      this.canvas.width = 250;
+      this.canvas.height = 250;
+      this.context = this.canvas.getContext('2d');
+      this.simplex = new SimplexNoise(4);
+      this.t = 0;
+      this.imagedata = this.context.getImageData(0,0,this.canvas.width,this.canvas.height);
+      this.data = this.imagedata.data;
+       
+    }
 
+    setNoiseMap() {
       const canvas = this.canvas;
       const c = this.context;
 
-      c.fillStyle = 'black'
-      c.fillRect(0,0,canvas.width, canvas.height)
-      for(let i=0; i<canvas.width; i++) {
-          for(let j=0; j<canvas.height; j++) {
-              let v =  octave(i/canvas.width,j/canvas.height,16)
-              const per = (100*v).toFixed(2)+'%'
-              c.fillStyle = `rgb(${per},${per},${per})`
-              c.fillRect(i,j,1,1)
+      for(let x=0; x<canvas.width; x++) {
+          for(let y=0; y<canvas.height; y++) {
+              var r = this.simplex.noise3D(x / 250, y / 250, this.t/50) * 0.4 + 0.4;
+
+              this.data[(x + y * canvas.width) * 4 + 0] = r * 255;
+              this.data[(x + y * canvas.width) * 4 + 1] = r * 255;
+              this.data[(x + y * canvas.width) * 4 + 2] = r * 255;
+              this.data[(x + y * canvas.width) * 4 + 3] = 255;
           }
       }
-      return c.getImageData(0,0,canvas.width,canvas.height)
-
+      this.t++;
+      c.putImageData(this.imagedata, 0, 0);
     }
 
     renderFrame() {
-      this.renderer.render( this.scene, this.camera );
+      //this.renderer.render( this.scene, this.camera );
+      this.setNoiseMap();
       requestAnimationFrame(this.renderFrame);
     } 
 }
