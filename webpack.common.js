8be2cdbd77eceac01cb43211fe9e451f756712b0
Your Name <you@example.com>
2021-11-26 19:18:58 +0800

    pages Fix up and add html webpack plugin and deploy gh pages

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,10 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'blobamatronix.js',
-    path: path.resolve(__dirname, 'lib'),
-    libraryTarget: 'var',
-    library: 'Blobamatronix'
+    path: path.resolve(__dirname, 'lib')
   },
   node: {
     fs: 'empty'
@@ -33,5 +31,22 @@ module.exports = {
           loader: 'webpack-glsl-loader'
         }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Blobamatronix Examples',
+      templateContent: ({htmlWebpackPlugin}) => `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <title>${htmlWebpackPlugin.options.title}</title>
+          </head>
+          <body style="margin: 0; padding: 0; width: 100%; height: 100vh;">
+          </body>
+        </html>
+      `
+    })
+  ],
 }
